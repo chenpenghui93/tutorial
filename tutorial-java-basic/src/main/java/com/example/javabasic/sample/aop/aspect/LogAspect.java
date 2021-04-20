2d152fbfd998c1d4e1ca6184cfdbf80111bf8395
chenpenghui93 <chenpenghui93@hotmail.com>
2021-04-20 16:42:06 +0800

    切面

--- tutorial-java-basic/src/main/java/com/example/javabasic/sample/aop/aspect/LogAspect.java
+++ tutorial-java-basic/src/main/java/com/example/javabasic/sample/aop/aspect/LogAspect.java
@@ -20,27 +20,25 @@ public class LogAspect {
 
     /**
      * 定义切入点：对要拦截的方法进行定义与限制，如指定包、指定类、指定方法
-     * <p>
+     *
      * 1、任意的公共方法
      * execution(public * *(..))
-     * <p>
+     *
      * 2、以set开头的所有的方法
      * execution(* set*(..))
-     * <p>
+     *
      * 3、com.example.javabasic.sample.aop.biz.testController这个类里的所有的方法
      * execution(* com.example.javabasic.sample.aop.biz.testController.* (..))
-     * <p>
+     *
      * 4、com.example.javabasic.sample.aop.biz包下的所有的类的所有的方法
      * execution(* com.example.javabasic.sample.aop.biz.*.*(..))
-     * <p>
+     *
      * 5、com.example.javabasic.sample.aop.biz包及子包下所有的类的所有的方法
      * execution(* com.example.javabasic.sample.aop.biz..*.*(..))
-     * <p>
-     * 6、com.example.javabasic.sample.aop.biz包及子包下所有的类的有三个参数，
-     * 第一个参数为String类型，第二个参数为任意类型，第三个参数为Long类型的方法
-     * <p>
+     *
+     * 6、com.example.javabasic.sample.aop.biz包及子包下所有的类的有三个参数，第一个参数为String类型，第二个参数为任意类型，第三个参数为Long类型的方法
      * @Pointcut("execution(* com.example.javabasic.sample.aop.biz..*.*(String,*,Long))")
-     * <p>
+     *
      * 7、任何标注了MyLog注解的目标类方法
      * @Pointcut("@annotation(com.example.javabasic.sample.aop.annotation.MyLog)")
      */
