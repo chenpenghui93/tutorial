4c77bd412deb09a1f04eaaa0a9e92b6a9e6c7b86
chenpenghui93 <chenpenghui93@hotmail.com>
2021-04-20 15:22:13 +0800

    Semaphore

--- tutorial-java-basic/src/main/java/com/example/javabasic/sample/concurrent/SemaphoreTest.java
+++ tutorial-java-basic/src/main/java/com/example/javabasic/sample/concurrent/SemaphoreTest.java
@@ -3,44 +3,33 @@ package com.example.javabasic.sample.concurrent;
 import java.util.concurrent.Semaphore;
 
 /**
+ * Semaphore 信号量，用于接口限流，控制并发数量
+ *
  * @author cph
  * @version 1.0
  * @date 2019/1/6
  */
 public class SemaphoreTest {
+
     public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(3);
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                try {
 
-        for (int i = 1; i <= 100; i++) {
-            new SecurityCheckedThread(i, semaphore).start();
-        }
-    }
+                    semaphore.tryAcquire();
+                    semaphore.isFair();
 
-    private static class SecurityCheckedThread extends Thread {
-        private int seq;
-        private Semaphore semaphore;
-
-        public SecurityCheckedThread(int seq, Semaphore semaphore) {
-            this.seq = seq;
-            this.semaphore = semaphore;
-        }
-
-        @Override
-        public void run() {
-            try {
-                semaphore.acquire();
-                System.out.println("NO." + seq + "乘客，正在查验中···");
-
-                if (seq % 2 == 0) {
-                    Thread.sleep(1000);
-                    System.out.println("NO." + seq + "乘客，身份可疑，不能出国！");
+                    semaphore.acquire();
+                    System.out.println("线程" + Thread.currentThread().getName() + "获得许可，当前已用：" + semaphore.drainPermits() + "，当前可用: " + semaphore.availablePermits());
+                    Thread.sleep(2000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                    System.out.println("线程" + Thread.currentThread().getName() + "释放许可");
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                semaphore.release();
-                System.out.println("NO." + seq + "乘客，已完成服务。");
-            }
+            }).start();
         }
     }
 }
