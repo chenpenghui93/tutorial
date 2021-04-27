e43f1242e9646b6da1999715f999b79e36a6d512
chenpenghui93 <chenpenghui93@hotmail.com>
2021-04-27 15:35:40 +0800

    lazy singleton

--- tutorial-design-pattern/src/main/java/com/example/designpattern/singleton/lazy/LazyDoubleCheckSingleton.java
+++ tutorial-design-pattern/src/main/java/com/example/designpattern/singleton/lazy/LazyDoubleCheckSingleton.java
@@ -6,13 +6,17 @@ package com.example.designpattern.singleton.lazy;
  */
 public class LazyDoubleCheckSingleton {
 
-    // 使用volatile解决指令重排序问题
+    /**
+     * 使用volatile解决指令重排序问题
+     */
     private volatile static LazyDoubleCheckSingleton lazy = null;
 
     private LazyDoubleCheckSingleton() {
     }
 
-    //使用双重检查锁的目的是提高性能，但因为jvm内存模型中无序写的问题，可能会失效
+    /**
+     * 使用双重检查锁的目的是提高性能，但因为jvm内存模型中无序写的问题，可能会失效
+     */
     public static LazyDoubleCheckSingleton getInstance() {
         //外层检查是为了让其它线程方法进来，减少锁的产生，提高性能
         //如果去掉外层if，其它线程方法就会进不来，效果与"将synchronized放在方法上"相同，达不到优化性能的目的
