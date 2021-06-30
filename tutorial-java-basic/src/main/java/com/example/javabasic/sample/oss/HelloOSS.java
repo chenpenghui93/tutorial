9fe2c2df294f7b7a4e439d610be2ddc9515f7b31
chenpenghui93 <chenpenghui93@hotmail.com>
2021-06-30 21:23:58 +0800

    阿里云OSS

---
+++ tutorial-java-basic/src/main/java/com/example/javabasic/sample/oss/HelloOSS.java
@@ -0,0 +1,13 @@
+package com.example.javabasic.sample.oss;
+
+/**
+ * @description:
+ * @author: cph
+ * @date: 2021/6/30 21:17
+ */
+public class HelloOSS {
+
+    public static void main(String[] args) {
+
+    }
+}
