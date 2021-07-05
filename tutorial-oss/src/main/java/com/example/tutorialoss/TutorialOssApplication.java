ac34bfc906366a97cc8751367d72bb96d34acab8
chenpenghui93 <chenpenghui93@hotmail.com>
2021-07-05 12:07:45 +0800

    阿里云OSS

---
+++ tutorial-oss/src/main/java/com/example/tutorialoss/TutorialOssApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialOssApplication.class, args);
+    }
+
+}
---
+++ tutorial-oss/src/test/java/com/example/tutorialoss/TutorialOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
