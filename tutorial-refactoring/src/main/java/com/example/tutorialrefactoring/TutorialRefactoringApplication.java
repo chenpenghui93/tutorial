3612547a3606a5c4641b56feef59f0396323928a
chenpenghui93 <chenpenghui93@hotmail.com>
2021-08-12 08:49:52 +0800

    add tutorial-refactoring

---
+++ tutorial-refactoring/src/main/java/com/example/tutorialrefactoring/TutorialRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialRefactoringApplication.class, args);
+    }
+
+}
