5338cc9bc7781be936cae3797c9dd56e53f32bb0
chenpenghui93 <chenpenghui93@hotmail.com>
2021-09-01 15:49:29 +0800

    RabbitMQ

---
+++ tutorial-rocketmq/src/main/java/com/example/tutorialrocketmq/TutorialRocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialRocketmqApplication.class, args);
+    }
+
+}
---
+++ tutorial-rocketmq/src/test/java/com/example/tutorialrocketmq/TutorialRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialrocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialRocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
